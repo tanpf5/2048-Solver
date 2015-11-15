0829d4ddd2509a518f49c3e5132168b815dc987a
Jelly-Yu <yu.ru@husky.neu.edu>
2015-11-14 19:20:04 -0500

    implemented GUI and initialization of game manager

--- 2048/src/GUI.java
+++ 2048/src/GUI.java
@@ -1,19 +1,24 @@
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.Box;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenuBar;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 public class GUI {
 
-	public static int WIDTH = 400;
-	public static int HEIGHT = 400;
+	public static int WIDTH = 406;
+	public static int HEIGHT = 459;
 	public static Color BG_COLOR = new Color(0xBBADA0);
 	public static Color EMPTY_TILE_COLOR = new Color(0xCDC1B4);
 	public static Color[] FONT_COLORS = new Color[]{ 
@@ -37,16 +42,25 @@ public class GUI {
 	public static Font FONT = new Font("Helvetica Neue",Font.BOLD,55);
 	
 	private JFrame frame;
-	public GUI(int width, int height, final GameManager gm){
+	public GUI(final int width, final int height, final GameManager gm){
 		frame = new JFrame();
 		frame.setTitle("2048-Solver");
 		JButton startBtn = new JButton("START");
-		JLabel score = new JLabel();
+		startBtn.setFocusPainted(false);
+		startBtn.addActionListener(new ActionListener(){
+			@Override
+			public void actionPerformed(ActionEvent e){
+				gm.start();
+			}
+			
+		});
+		final JLabel score = new JLabel();
 		score.setText("Score: 0");
 		score.setBorder(BorderFactory.createEmptyBorder(0,0,0,10));
 		
 		JMenuBar menuBar = new JMenuBar();
 		menuBar.add(startBtn);
+		menuBar.add(Box.createHorizontalGlue());
 		menuBar.add(score);
 		
 		JPanel gridPanel = new JPanel(){
@@ -62,35 +76,90 @@ public class GUI {
 				int[][] cells = gm.getCells();
 				Graphics2D graphics = (Graphics2D) g.create();
 				graphics.setColor(getColor(BG_COLOR, 1.0f));
-				graphics.fillRoundRect(0, 0, getWidth(), getHeight(), 10, 10);
-				int tileOuterWidth = getWidth()/cells[0].length;
-				int tileOuterHeight = getHeight()/cells.length;
-				int tileInnerWidth = (int) (0.8d*tileOuterWidth);
-				int tileInnerHeight = (int) (0.8d*tileOuterHeight);
-				int widthPadding = (int) (0.1d * tileOuterWidth);
-				int heightPadding = (int) (0.1d * tileOuterHeight);
+				graphics.fillRoundRect(0, 0, getWidth(), getHeight(), 0, 0);
+				//System.out.println("width: "+getWidth());
+				//System.out.println("height: "+getHeight());
+				int tileInnerWidth = (int) (0.2d*getWidth());
+				//System.out.println("innerWidth"+tileInnerWidth);
+				int tileInnerHeight = (int) (0.2d*getHeight());
+				//System.out.println("tileInnerHeight"+tileInnerHeight);
+				int widthPadding = (int) (0.2d * tileInnerWidth);
+				int heightPadding = (int) (0.2d * tileInnerHeight);
 				for(int r = 0; r<cells.length; r++){
 					for(int c = 0; c<cells[r].length;c++){
-						if(cells[r][c] == GameManager.EMPTY_TILE){
+						int position = (int) (Math.log(cells[r][c])/Math.log(2));
+						//set background color of each tile
+						if(cells[r][c] == 0){
 							graphics.setColor(getColor(EMPTY_TILE_COLOR,1.0f));
 						}
 						else{
+							graphics.setColor(getColor(TILE_BG_COLORS[position-1],1.0f));
+						}
+						int xPos = (c+1)*widthPadding + c*tileInnerWidth;
+						int yPos = (r+1)*heightPadding+ r*tileInnerHeight;
+						graphics.fillRoundRect(xPos, yPos, tileInnerWidth, tileInnerHeight, 3, 3);
+						
+						//if the cell is not an empty tile, set specific font color and text.
+						if(cells[r][c] > 0){
+							if(position == 1 || position == 2){
+								graphics.setColor(getColor(FONT_COLORS[0],1.0f));
+							}
+							else{
+								graphics.setColor(getColor(FONT_COLORS[1],1.0f));
+							}
+							String text = String.valueOf(cells[r][c]);
+							graphics.setFont(FONT);
+							FontMetrics fm = g.getFontMetrics(graphics.getFont());
+							int textWidth = fm.stringWidth(text);
+							int textHeight = fm.getHeight();
+							graphics.drawString(text, (int)((xPos + 0.5d * tileInnerWidth) - (textWidth * 0.5d)), (int)((yPos + 0.5d * tileInnerHeight)+(textHeight * 0.3d)));
+							
 							
 						}
 					}
 				}
+				
+				score.setText("Score: " + String.valueOf(gm.getScore()));
 				
 				
 			}
 			
 			
 		};
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setContentPane(gridPanel);
+		frame.setJMenuBar(menuBar);
+		frame.setSize(width, height);
+		frame.setVisible(true);
+		frame.setResizable(false);
+		frame.setLocationRelativeTo(null);
+	}
+	public GUI(GameManager gm){
+		this(WIDTH,HEIGHT, gm);
 	}
 	
+	public void update(){
+		frame.revalidate();
+		frame.repaint();
+	}
 	private Color getColor(Color color, float alpha){
 		float[] ingredients = color.getRGBComponents(null);
 		return new Color(ingredients[0],ingredients[1],ingredients[2],alpha);
 	}
 
+	public static void main(String[] args){
+		GameManager gm = new GameManager(4,4);
+		final GUI gui = new GUI(gm);
+		ActionListener taskController = new ActionListener(){
+
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// TODO Auto-generated method stub
+				gui.update();
+			}
+		};
+		new Timer(100, taskController).start();
+	}
 
 }
--- 2048/src/GameManager.java
+++ 2048/src/GameManager.java
@@ -1,4 +1,4 @@
-
+import java.util.Random;
 
 public class GameManager {
 	public static int NUM_OF_ROWS = 4;
@@ -28,5 +28,26 @@ public class GameManager {
 	}
 	public void setScore(int score){
 		this.score = score;
+	}
+	public void start(){
+		for(int r = 0; r< cells.length;r++){
+			for(int c = 0; c<cells[r].length; c++){
+				cells[r][c] = EMPTY_TILE;
+			}
+		}
+		score = 0;
+		Random random = new Random();
+		int randomX = 0;
+		int randomY = 0;
+		int randomX2 = 0;
+		int randomY2 = 0;
+		while(randomX == randomX2 && randomY == randomY2){
+			randomX = random.nextInt(NUM_OF_ROWS);
+			randomY = random.nextInt(NUM_OF_COLS);
+			randomX2 = random.nextInt(NUM_OF_ROWS);
+			randomY2 = random.nextInt(NUM_OF_COLS);			
+		}
+		cells[randomX][randomY] = BASIC_TILES[0];
+		cells[randomX2][randomY2] = BASIC_TILES[0];
 	}
 }
